b0659064c2299c4f81dedff7c9fd4eb6956d78c5
Gustavo <gguaimas85@gmail.com>
2025-10-30 14:28:26 -0300

    puerto 3000

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ const HOST = "127.0.0.1";
 server.use(express.json());
 server.use(cors());
 server.use("/api", router);
+server.get("/", (req, res) => res.send("Corriendo server"));
 
 /*server.listen(PORT, HOST, () => {
   console.log(`Server running at http://${HOST}:${PORT}/`);
 });*/
 
-server.listen(PORT);
-console.log(`Server running at port:${PORT}/`);
+server.listen(PORT, () => {
+  console.log(`Server running at port:${PORT}`);
+});
